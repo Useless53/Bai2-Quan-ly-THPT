061e2f058945349e5ea2d04c665de8dde5244257
thanhphonghy135 <thanhphonghy135@gmail.com>
2020-10-09 23:49:17 +0700

    Module chính

---
+++ Solution/QuanLyTHPT/QuanLyTHPT/Form1.designer.cs
@@ -0,0 +1,115 @@
+﻿namespace QuanLyTHPT
+{
+    partial class frm_Start
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_Start));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.button1.Image = global::QuanLyTHPT.Properties.Resources.classroom;
+            this.button1.Location = new System.Drawing.Point(374, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 153);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "LỚP";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.button2.Image = global::QuanLyTHPT.Properties.Resources.student_2;
+            this.button2.Location = new System.Drawing.Point(102, 1);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 153);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "HỌC SINH";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.button3.Image = global::QuanLyTHPT.Properties.Resources.teacher;
+            this.button3.Location = new System.Drawing.Point(238, 1);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(130, 153);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "GIÁO VIÊN";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.button4.Image = global::QuanLyTHPT.Properties.Resources.guide;
+            this.button4.Location = new System.Drawing.Point(510, 1);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(130, 153);
+            this.button4.TabIndex = 3;
+            this.button4.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // frm_Start
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundImage = global::QuanLyTHPT.Properties.Resources.QuanLyTHPT;
+            this.ClientSize = new System.Drawing.Size(751, 498);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frm_Start";
+            this.Text = "Quản lý THPT";
+            this.Load += new System.EventHandler(this.frm_Start_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+
+
+
+    }
+}
+
